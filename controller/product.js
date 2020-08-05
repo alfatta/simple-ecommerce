27ed3aa2d374ae70c790f76112380c7d35269533
alfatta <arwinnersyah@gmail.com>
2020-08-05 21:24:17 +0700

    add get product filtering

--- controller/product.js
+++ controller/product.js
@@ -2,31 +2,79 @@ const responseBuilder = require('../helper/response')
 
 module.exports.getAll = (req, res) => {
   // filter
-  // - range harga
-  // - kondisi barang
+  // - range harga maxPrice minprice
+  // - kondisi barang condition
   // search (/product?q=mainan)
   // sorting
   // - dinamis (/product?sort=price&order=asc)
   // paginate (/product?page=4&perpage=10)
   // - meta : page, perpage, lastPage, firstPage, prevPage, nextPage
 
-  const sql = 'SELECT * FROM `product` WHERE deleted_at IS NULL'
+  let sql = 'SELECT * FROM `product` WHERE deleted_at IS NULL'
 
-  req.db.query(sql, (err, result) => {
-    if (err) {
-      res.send(
-        responseBuilder(0, err.message)
-      )
-      console.log(err.message);
-    } else {
-      console.log('RESULT : ', result);
-      res.send(
-        responseBuilder(1, null, result, {
-          count: result.length
-        })
-      )
+  let {
+    maxPrice, minPrice, condition, q, sort, order, page, perPage
+  } = req.query
+  if (maxPrice && minPrice) {
+    sql += ` AND price BETWEEN ${minPrice} AND ${maxPrice}`
+  }
+
+  if (condition) {
+    sql += ` AND \`condition\` = ${condition}`
+  }
+
+  if (q) {
+    sql += ` AND name LIKE '%${q}%'`
+  }
+
+  if (sort) {
+    if (!order) order = 'asc'
+    sql += ` ORDER BY ${sort} ${order}`
+  } else {
+    sql += ` ORDER BY created_at desc`
+  }
+
+  
+
+  let sql2 = sql.replace('*', 'COUNT(*) as count')
+
+  console.log(sql2);
+  
+  req.db.query(sql2, (err2, result2) => {
+    const totalData = result2[0].count
+
+    if (page) {
+      if (!perPage) perPage = 20
+      const offset = (page - 1) * perPage
+      sql += ` LIMIT ${offset}, ${perPage}`
     }
+
+    console.log(sql);
+    req.db.query(sql, (err, result) => {
+      if (err) {
+        res.send(
+          responseBuilder(0, err.message)
+        )
+        console.log(err.message);
+      } else {
+        console.log('RESULT : ', result);
+        res.send(
+          responseBuilder(1, null, result, {
+            count: result.length,
+            total: totalData,
+            page: parseInt(page),
+            perPage: parseInt(perPage),
+            totalPage: Math.ceil(totalData / perPage),
+            firstPage: 1,
+            lastPage: Math.ceil(totalData / perPage),
+            prevPage: parseInt(page) - 1 < 1 ? null : parseInt(page) - 1,
+            nextPage: (parseInt(page) + 1) > Math.ceil(totalData / perPage) ? null : (parseInt(page) + 1)
+          })
+        )
+      }
+    })
   })
+  
 }
 
 module.exports.getById = (req, res) => {
